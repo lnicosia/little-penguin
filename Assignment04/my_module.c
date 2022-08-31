126dafe40a5bcdbb1de97faa47f662c84198a9cb
lnicosia <lnicosia@student.42.fr>
2022-08-31 10:22:29 +0200

    [MOD] Change log message to identify our module easier.

--- Assignment04/my_module.c
+++ Assignment04/my_module.c
@@ -11,13 +11,13 @@ MODULE_DESCRIPTION("USB keyboard loading.");
 static int test_probe(struct usb_interface *interface,
 			const struct usb_device_id *id)
 {
-	pr_info("USB Keyboard plugged in");
+	pr_info("USB Keyboard mega cool driver plugged in");
 	return 0;
 }
 
 static void test_disconnect(struct usb_interface *interface)
 {
-	pr_info("USB Keyboard unplugged");
+	pr_info("USB Keyboard mega cool driver unplugged");
 }
 
 static struct usb_device_id id_table_test [] = {
@@ -38,14 +38,14 @@ static struct usb_driver my_driver = {
 
 static int __init	usb_module_init(void)
 {
-	pr_info("USB Keyboard module loaded !\n");
+	pr_info("USB Keyboard mega cool module loaded !\n");
 	usb_register(&my_driver);
 	return 0;
 }
 
 static void __exit	usb_module_cleanup(void)
 {
-	pr_info("Cleaning up USB Keyboard module.\n");
+	pr_info("Cleaning up USB Keyboard mega cool module.\n");
 	usb_deregister(&my_driver);
 }
 
