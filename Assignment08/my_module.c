c6edcd195a51410751d93b35a4b9b18caf153d82
lnicosia <lnicosia@student.42.fr>
2022-08-31 18:11:31 +0200

    [ADD] Assignment08 wip.

--- Assignment08/my_module.c
+++ Assignment08/my_module.c
@@ -34,34 +34,35 @@ static int __init myfd_init(void)
 	int retval;
 
 	retval = misc_register(&myfd_device);
-	return 1;
+	if (retval != 0) {
+		pr_err("Could not register misc device\n");
+		return retval;
+	}
+	return 0;
 }
 
 static void __exit myfd_cleanup(void)
 {
+	misc_deregister(&myfd_device);
 }
 
 ssize_t myfd_read(struct file *fp, char __user *user, size_t size,
 		loff_t *offs)
 {
-	size_t t, i;
-	char *tmp2;
-
-	tmp2 = kmalloc(sizeof(char) * PAGE_SIZE * 2, GFP_KERNEL);
-	tmp = tmp2;
-	for (t = strlen(str) - 1, i = 0; t >= 0; t--, i++)
-		tmp[i] = str[t];
-	tmp[i] = 0x0;
-	return simple_read_from_buffer(user, size, offs, tmp, i);
+	return simple_read_from_buffer(user, size, offs, str, PAGE_SIZE);
 }
 
 ssize_t myfd_write(struct file *fp, const char __user *user, size_t size,
 		loff_t *offs)
 {
 	ssize_t res;
-	res = 0;
-	res = simple_write_to_buffer(str, size, offs, user, size) + 1;
-	str[size + 1] = 0x0;
+
+	res = simple_write_to_buffer(str, PAGE_SIZE, offs, user, size);
+	if (res < PAGE_SIZE) {
+		str[res + 1] = 0;
+		return res + 1;
+	}
+	str[PAGE_SIZE - 1] = 0;
 	return res;
 }
 
