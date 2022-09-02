17713f745d25ff43304ed56a49f7331f0114c070
lnicosia <lnicosia@student.42.fr>
2022-09-02 16:36:46 +0200

    [ADD] Assignment09 done.

--- Assignment09/mymounts.c
+++ Assignment09/mymounts.c
@@ -5,70 +5,56 @@
 #include <linux/nsproxy.h>
 #include <linux/mount.h>
 #include <linux/list.h>
+#include <linux/limits.h>
 #include <../fs/mount.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Lucas Nicosia <lnicosia@student.42.fr>");
 MODULE_DESCRIPTION("Mountpoints listing module.");
 
-/*struct mount {
-	struct list_head		mnt_hash;
-	struct mount			*mnt_parent;
-	struct dentry			*mnt_mountpoint;
-	struct vfsmount			mnt;
-	int						mnt_count;
-	int						mnt_writers;
-	struct list_head		mnt_mounts;
-	struct list_head		mnt_child;
-	struct list_head		mnt_instance;
-	const char				*mnt_devname;
-	struct list_head		mnt_list;
-	struct list_head		mnt_expire;
-	struct list_head		mnt_share;
-	struct list_head		mnt_slave_list;
-	struct list_head		mnt_slave;
-	struct mount			*mnt_master;
-	struct mnt_namespace	*mnt_ns;
-	int						mnt_id;
-	int						mnt_group_id;
-	int						mnt_expiry_mark;
-	int						mnt_pinned;
-	int						mnt_ghosts;
+struct mounts_output {
+	int		padding;
+	size_t	total_len;
 };
 
-struct mnt_namespace {
-	atomic_t			count;
-	struct mount		*root;
-	struct list_head	list;
-	wait_queue_head_t	poll;
-	int					event;
-};*/
-
 static struct proc_dir_entry *mymounts;
+static struct mounts_output mnt_output;
+static char *out_buf;
+
+static struct mounts_output get_mount_output(struct mnt_namespace *ns)
+{
+	struct mount *mnt;
+	struct mounts_output res;
+	struct path mnt_path;
+	size_t len;
+	size_t max_devname_len;
+	char bpath[1024];
+	char *p;
+
+	memset(&res, 0, sizeof res);
+	max_devname_len = 0;
+	list_for_each_entry(mnt, &ns->list, mnt_list) {
+		if (mnt->mnt_id != 1) {
+			len = strlen(mnt->mnt_devname);
+			if (len > max_devname_len)
+				max_devname_len = len;
+			mnt_path.mnt = &mnt->mnt;
+			mnt_path.dentry = mnt->mnt.mnt_root;
+			p = d_path(&mnt_path, bpath, 1024);
+			res.total_len += strlen(p);
+		}
+	}
+	if (max_devname_len < INT_MAX - 8)
+		res.padding = max_devname_len + 8;
+	/* padding + 1 for the \n on every line */
+	res.total_len += (res.padding + 1) * (ns->mounts - 1);
+	return res;
+}
 
 static ssize_t list_mountpoints(struct file *file, char __user *buf,
-		size_t len, loff_t *ppos)
+		size_t count, loff_t *f_pos)
 {
-	char bpath[1024];
-	char *p = NULL;
-	struct mnt_namespace *ns = current->nsproxy->mnt_ns;
-	struct mount *mnt;
-	struct path mnt_path;
-
-	memset(bpath, 0, 1024);
-	pr_info("Listing mountpoints\n");
-	list_for_each_entry(mnt, &ns->list, mnt_list) {
-   		mnt_path.mnt = &mnt->mnt;
-		mnt_path.dentry = mnt->mnt.mnt_root;
-		p = d_path(&mnt_path, bpath, 1024);
-		if (IS_ERR(p)) {
-			pr_info("d_path() failed\n");
-			return -1;
-		}
-		if (mnt->mnt_id != 1)
-			pr_info("%s\t\t%s\n", mnt->mnt_devname, p);
-	}
-	return 0;
+	return simple_read_from_buffer(buf, count, f_pos, out_buf, mnt_output.total_len);
 }
 
 static struct proc_ops mountlist_ops = {
@@ -77,9 +63,39 @@ static struct proc_ops mountlist_ops = {
 
 static int __init	mymounts_init(void)
 {
+	char bpath[1024];
+	char *p = NULL;
+	struct mnt_namespace *ns = current->nsproxy->mnt_ns;
+	struct mount *mnt;
+	struct path mnt_path;
+	int ret;
+
 	if ((mymounts = proc_create("mymounts", 0444, NULL, &mountlist_ops)) == NULL) {
 		pr_err("Could not create /proc file\n");
 		return -1;
+	}
+	ret = 0;
+	memset(bpath, 0, 1024);
+	pr_info("Listing %d mountpoints\n", ns->mounts - 1);
+	mnt_output = get_mount_output(ns);
+	if (!(out_buf = kmalloc(mnt_output.total_len,
+					GFP_KERNEL))) {
+		pr_err("Could not kmalloc output_buffer\n");
+		return -1;
+	}
+	list_for_each_entry(mnt, &ns->list, mnt_list) {
+   		mnt_path.mnt = &mnt->mnt;
+		mnt_path.dentry = mnt->mnt.mnt_root;
+		p = d_path(&mnt_path, bpath, 1024);
+		if (IS_ERR(p)) {
+			pr_info("d_path() failed\n");
+			return -1;
+		}
+		if (mnt->mnt_id != 1) {
+			pr_info("%-*s%s\n", mnt_output.padding, mnt->mnt_devname, p);
+			ret += sprintf(out_buf + ret, "%-*s%s\n",
+					mnt_output.padding, mnt->mnt_devname, p);
+		}
 	}
 
 	pr_info("Mountpoints listing module loaded !\n");
@@ -88,6 +104,7 @@ static int __init	mymounts_init(void)
 
 static void __exit	mymounts_cleanup(void)
 {
+	kfree(out_buf);
 	proc_remove(mymounts);
 	pr_info("Cleaning up mountpoints listing module.\n");
 }
