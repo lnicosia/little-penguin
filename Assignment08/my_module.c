d2bbf1d60dd7cfed1610cfbf313a379735e32e82
lnicosia <lnicosia@student.42.fr>
2022-09-05 16:43:55 +0200

    [FIX-Assignment08] Working.

--- Assignment08/my_module.c
+++ Assignment08/my_module.c
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: GPL
+
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/init.h>
@@ -5,7 +7,7 @@
 #include <linux/fs.h>
 #include <linux/slab.h>
 
-MODULE_LICENSE("GLP");
+MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Louis Solofrizzo <louis@ne02ptzero.me>");
 MODULE_DESCRIPTION("Useless module");
 
@@ -14,7 +16,7 @@ static ssize_t myfd_read(struct file *fp, char __user *user,
 static ssize_t myfd_write(struct file *fp, const char __user *user,
 		size_t size, loff_t *offs);
 
-static struct file_operations myfd_fops = {
+static const struct file_operations myfd_fops = {
 	.owner = THIS_MODULE,
 	.read = &myfd_read,
 	.write = &myfd_write
@@ -27,7 +29,7 @@ static struct miscdevice myfd_device = {
 };
 
 char str[PAGE_SIZE];
-char *tmp;
+char tmp[PAGE_SIZE];
 
 static int __init myfd_init(void)
 {
@@ -49,7 +51,22 @@ static void __exit myfd_cleanup(void)
 ssize_t myfd_read(struct file *fp, char __user *user, size_t size,
 		loff_t *offs)
 {
-	return simple_read_from_buffer(user, size, offs, str, PAGE_SIZE);
+	size_t t;
+	size_t i;
+	size_t len;
+
+	len = strlen(str);
+	if (len == 0)
+		return 0;
+	t = len - 1;
+	i = 0;
+	while (i < len) {
+		tmp[i] = str[t];
+		t--;
+		i++;
+	}
+	tmp[i] = 0;
+	return simple_read_from_buffer(user, size, offs, tmp, i);
 }
 
 ssize_t myfd_write(struct file *fp, const char __user *user, size_t size,
@@ -58,9 +75,9 @@ ssize_t myfd_write(struct file *fp, const char __user *user, size_t size,
 	ssize_t res;
 
 	res = simple_write_to_buffer(str, PAGE_SIZE, offs, user, size);
-	if (res < PAGE_SIZE) {
-		str[res + 1] = 0;
-		return res + 1;
+	if (res >= 0 && res < PAGE_SIZE) {
+		str[res] = 0;
+		return res;
 	}
 	str[PAGE_SIZE - 1] = 0;
 	return res;
